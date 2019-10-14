e9a86ccec2f8fc8c7eca1bac29f2698e6915c7f2
Frank Gerold <franketsgerold@gmail.com>
2019-10-14 20:54:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,12 @@ def reduce_to_total (array, starting_point = 0)
   starting_point
 end
 
+def reduce_to_all_true (array)
+  array.length.times do |x|
+    if array[x] == false 
+      return false 
+    end
+  end
+  true
+end
+
