ea1efab18d549ef7a4a964fb8b00ae10585364c9
Frank Gerold <franketsgerold@gmail.com>
2019-10-14 20:52:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
-# My Code here....
+def map_to_negativize (array)
+  mapped = []
+  array.length.times do |x|
+    mapped[x] = array[x] * -1
+  end
+  mapped
+end
+
+def map_to_no_change (array)
+  mapped = []
+  array.length.times do |x|
+    mapped[x] = array[x]
+  end
+  mapped
+end
+
+def map_to_double (array)
+  mapped = []
+  array.length.times do |x|
+    mapped[x] = array[x] * 2
+  end
+  mapped
+end
+
+def map_to_square (array)
+  mapped = []
+  array.length.times do |x|
+    mapped[x] = array[x] ** 2
+  end
+  mapped
+end
+
+def reduce_to_total (array, starting_point = 0)
+  array.length.times do |x|
+    starting_point += array[x]
+  end
+  starting_point
+end
+
